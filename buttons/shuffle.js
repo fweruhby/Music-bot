2a1cba2b7839a1b3ae7ef1931b78310e8b98c23c
fweruhby <verganqwj@gmail.com>
2024-10-12 16:43:35 +0200

    Update shuffle.js

--- buttons/shuffle.js
+++ buttons/shuffle.js
@@ -2,8 +2,8 @@ const { EmbedBuilder } = require('discord.js');
 const { Translate } = require('../process_tools');
 
 module.exports = async ({ inter, queue }) => {
-    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing... try again ? <❌>`) });
-    if (!queue.tracks.toArray()[0]) return inter.editReply({ content: await Translate(`No music in the queue after the current one <${inter.member}>... try again ? <❌>`) });
+    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`Nessuna musica attualmente in riproduzione... riprovare? <❌>`) });
+    if (!queue.tracks.toArray()[0]) return inter.editReply({ content: await Translate(`Nessuna musica in coda dopo quella corrente <${inter.member}> <❌>`) });
 
     await queue.tracks.shuffle();
 
