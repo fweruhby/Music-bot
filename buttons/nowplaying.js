67d5dd259d4cb5fd39f7b8c632e102b1d0c3cc2f
fweruhby <verganqwj@gmail.com>
2024-10-12 16:40:18 +0200

    Update nowplaying.js

--- buttons/nowplaying.js
+++ buttons/nowplaying.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const { Translate } = require('../process_tools');
 
 module.exports = async ({ client, inter, queue }) => {
-    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing... try again ? <❌>`) });
+    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`Nessuna musica attualmente in riproduzione... riprovare? <❌>`) });
 
     const track = queue.currentTrack;
     const methods = ['disabled', 'track', 'queue'];
@@ -14,7 +14,7 @@ module.exports = async ({ client, inter, queue }) => {
         .setAuthor({ name: track.title, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
         .setThumbnail(track.thumbnail)
         .setDescription(await Translate(`Volume <**${queue.node.volume}**%\n> <Duration **${trackDuration}**\n> <Progress ${progress}\n> <Loop mode **${methods[queue.repeatMode]}**\n> <Requested by ${track.requestedBy}>`))
-        .setFooter({ text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: inter.member.avatarURL({ dynamic: true }) })
+        .setFooter({ text: 'La musica viene prima di tutto - Fatto con il cuore dagli sviluppatori ❤️', iconURL: inter.member.avatarURL({ dynamic: true }) })
         .setColor('ff0000')
         .setTimestamp();
 
