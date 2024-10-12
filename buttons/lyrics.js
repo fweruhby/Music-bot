a29dd460fcd0fb837788aef294e2de3072a6be55
fweruhby <verganqwj@gmail.com>
2024-10-12 16:39:10 +0200

    Update lyrics.js

--- buttons/lyrics.js
+++ buttons/lyrics.js
@@ -4,7 +4,7 @@ const { Translate } = require('../process_tools');
 
 module.exports = async ({ inter, queue }) => {
     const player = useMainPlayer();
-    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`No music currently playing <${inter.member}>... try again ? <❌>`) });
+    if (!queue?.isPlaying()) return inter.editReply({ content: await Translate(`Nessuna musica attualmente in riproduzione <${inter.member}> <❌>`) });
 
     const results = await player.lyrics
         .search({
@@ -12,11 +12,11 @@ module.exports = async ({ inter, queue }) => {
         })
         .catch(async (e) => {
             console.log(e);
-            return inter.editReply({ content: await Translate(`Error! Please contact Developers! | <❌>`) });
+            return inter.editReply({ content: await Translate(`Errore! Contatta gli sviluppatori! | <❌>`) });
         });
 
     const lyrics = results?.[0];
-    if (!lyrics?.plainLyrics) return inter.editReply({ content: await Translate(`No lyrics found for <${queue.currentTrack.title}>... try again ? <❌>`) });
+    if (!lyrics?.plainLyrics) return inter.editReply({ content: await Translate(`Nessun testo trovato per <${queue.currentTrack.title}> <❌>`) });
 
     const trimmedLyrics = lyrics.plainLyrics.substring(0, 1997);
 
@@ -26,7 +26,7 @@ module.exports = async ({ inter, queue }) => {
             name: lyrics.artistName
         })
         .setDescription(trimmedLyrics.length === 1997 ? `${trimmedLyrics}...` : trimmedLyrics)
-        .setFooter({ text: await Translate('Music comes first - Made with heart by the Community <❤️>'), iconURL: inter.member.avatarURL({ dynamic: true }) })
+        .setFooter({ text: await Translate('La musica viene prima di tutto - Realizzata con cuore dalla Community <❤️>'), iconURL: inter.member.avatarURL({ dynamic: true }) })
         .setTimestamp()
         .setColor('#2f3136');
 
